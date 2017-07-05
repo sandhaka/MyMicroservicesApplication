eaa4452af9fa7a0982ceeca428cc2e689e0d2d6a
Matteo Tosato <matteo.tosato7@gmail.com>
2017-07-05 14:44:59 +0200

    changes to docker-compose

--- Services/AuthService/Controllers/SecretsController.cs
+++ Services/AuthService/Controllers/SecretsController.cs
@@ -6,18 +6,6 @@ namespace AuthService.Controllers
     [Route("api/[controller]")]
     public class SecretsController : Controller
     {
-        [HttpGet("public-key")]
-        public string Get()
-        {
-            if(System.IO.File.Exists("/run/secrets/jwt-pub-key"))
-                return System.IO.File.ReadAllText("/run/secrets/jwt-pub-key");
-            return "File not found";
-        }
-
-        [HttpPost]
-        public IActionResult Login([FromBody] LoginDto loginDto)
-        {
-            
-        }
+        
     }
 }
