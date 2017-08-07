cc98cba7de8fd607081ceff09543b037c0085bbd
Matteo Tosato <matteo.tosato7@gmail.com>
2017-08-07 12:38:32 +0200

    Connect Catalog service via EventBus

---
+++ Services/Catalog/Catalog.Application/IntegrationEvents/Events/OrderStartedIntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using EventBus.Events;
+
+namespace Catalog.Application.IntegrationEvents.Events
+{
+    [DataContract]
+    public class OrderStartedIntegrationEvent : IntegrationEvent
+    {
+        [DataMember]
+        public int OrderId { get; set; }
+
+        [DataMember]
+        public Dictionary<int, int> OrderItems { get; set; }
+
+        public OrderStartedIntegrationEvent(Guid guid, DateTime creation, int orderId, Dictionary<int,int> orderItems) 
+            : base(guid, creation)
+        {
+            OrderId = orderId;
+            OrderItems = orderItems;
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/IntegrationEvents/OrderStartedIntegrationEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Catalog.Application.IntegrationEvents.Events;
+using EventBus.Abstractions;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Application.IntegrationEvents
+{
+    public class OrderStartedIntegrationEventHandler : IIntegrationEventHandler<OrderStartedIntegrationEvent>
+    {
+        private readonly ILogger _logger;
+
+        public OrderStartedIntegrationEventHandler(ILogger<OrderStartedIntegrationEventHandler> iLogger)
+        {
+            _logger = iLogger;
+        }
+        
+        public Task Handle(OrderStartedIntegrationEvent @event)
+        {
+            _logger.LogInformation($"Received integration event Order started. " +
+                                   $"OrderId: {@event.OrderId}, " +
+                                   $"Creation date: {@event.CreationDate}");
+            
+            // TODO: Remove items quantity from the catalog
+
+            return Task.CompletedTask;
+        }
+    }
+}
