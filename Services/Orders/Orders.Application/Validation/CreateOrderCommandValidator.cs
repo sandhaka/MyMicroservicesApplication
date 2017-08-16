910acd8201086aa7807213b4a792c3f64a01b7e0
Matteo Tosato <matteo.tosato7@gmail.com>
2017-08-16 18:51:23 +0200

    CreateOrderCommand validation completed

--- Services/Orders/Orders.Application/Validation/CreateOrderCommandValidator.cs
+++ Services/Orders/Orders.Application/Validation/CreateOrderCommandValidator.cs
@@ -1,5 +1,9 @@
-﻿using FluentValidation;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation;
 using Orders.Application.Commands;
+using Orders.Application.Controllers;
 
 namespace Orders.Application.Validation
 {
@@ -7,7 +11,22 @@ namespace Orders.Application.Validation
     {
         public CreateOrderCommandValidator()
         {
-            RuleFor(c => c.OrderItems).NotNull().NotEmpty();
+            RuleFor(c => c.OrderItems).Must(ContainOrderItems).WithMessage("No order items found");
+            RuleFor(c => c.CardNumber).NotEmpty().Length(12, 19);
+            RuleFor(c => c.CardHolderName).NotEmpty();
+            RuleFor(c => c.CardExpiration).NotEmpty().Must(BeValidExpirationDate)
+                .WithMessage("Please specify a valid card expiration date");
+            RuleFor(c => c.CardSecurityNumber).NotEmpty().Length(3);
+        }
+        
+        private bool BeValidExpirationDate(DateTime dateTime)
+        {
+            return dateTime >= DateTime.UtcNow;
+        }
+        
+        private bool ContainOrderItems(IEnumerable<OrderItemDto> orderItems)
+        {
+            return orderItems.Any();
         }
     }
 }
